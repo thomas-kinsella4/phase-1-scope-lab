98dce70e20e9ed8d6f3f4e3cb7e727511d174af0
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-08-09 19:46:51 -0400

    commit tree 972527a44b47af5588c1dc950b8b5dc3563d6ffe

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution in this file!
+// Write your solution in this file!\
+// var customerName = "bob";
+
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+// }
+
+// var bestCustomer;
+
+// function setBestCustomer() {
+//     bestCustomer = "not bob";
+// }
+
+// function overwriteBestCustomer() {
+//     bestCustomer = "maybe bob";
+// }
+
+// const leastFavoriteCustomer = "bob";
+
+// function changeLeastFavoriteCustomer() {
+//     leastFavoriteCustomer = "bub";
+// }
+
+
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "me";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not me";
+}
